6fd85dba6a2c444ab03358b751e93b2fa6f1acbb
Wira Azmoon <wira.azmoon@gmail.com>
2020-02-11 11:35:46 +0800

    Correct sim

--- Kattis/sim.cpp
+++ Kattis/sim.cpp
@@ -1,44 +1,56 @@
 #include <iostream>
 #include <string>
+#include <list>
+#include <stack>
 
 using namespace std;
 
+void empty_stack(list<char> &res, stack<char> &stk) {
+	while (!stk.empty()) {
+		res.push_front(stk.top());
+		stk.pop();
+	}
+}
 
-void special(string &s, string &res, char &c, int *idx) {
-	if (c == '<' && *idx) {
-		if (*idx == -1)
+void special(list<char> &res, stack<char> &stk, char &c, int *idx) {
+	if (c == '<') {
+		if (*idx == 0 && !stk.empty())
+			stk.pop();
+		else if (*idx == -1 && !res.empty())
 			res.pop_back();
-		else {
-			res.erase(res.begin() + *idx - 1);
-			(*idx)--;
-		}
+	}
+	// when pressing home, empty stack
+	else if (c == '[') {
+		empty_stack(res, stk);
+		*idx = 0;
 	}
 	else if (c == ']')
 		*idx = -1;
-	else if (c == '[')
-		*idx = 0;
+	// after pressing end
+	else if (*idx == -1) {
+		empty_stack(res, stk);
+		res.push_back(c);
+	}
+	// add to stack
+	else
+		stk.push(c);
 }
 
 void decode() {
-	string s, res;
+	string s;
+	list<char> res;
+	stack<char> stk;
 	char c;
-	int idx = 0;
+	int idx = -1;
 	getline(cin, s);
 
-	for (int i = 0; i < s.size(); i++) {
-		c = s[i];
-		if (c == '<' || c == ']' || c == '[')
-			special(s, res, c, &idx);
-		else if (idx < 0)
-			res.push_back(c);
-		else {
-			res.insert(res.begin() + idx, c);
-			idx++;
-		}
+	for (auto& c: s) {
+		special(res, stk, c, &idx);
 	}
-
-	cout << res << '\n';
-	
+	empty_stack(res, stk);
+	for (auto& c : res)
+		cout << c;
+	cout << '\n';
 }
 
 void sim() {
@@ -50,9 +62,11 @@ void sim() {
 	}
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    sim();
-    return 0;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	sim();
+
+	return 0;
 }
