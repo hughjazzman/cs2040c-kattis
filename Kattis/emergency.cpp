9b0032219c01067ec495bbc11f4db2503356be72
Wira Azmoon <wira.azmoon@gmail.com>
2020-04-24 00:19:26 +0800

    Add emer

---
+++ Kattis/emergency.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+void emer() {
+	ll n, k;
+	cin >> n >> k;
+	if (--n < 2 * k)
+		cout << n;
+	else
+		cout << k + 1 + n % k;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    emer();
+    return 0;
+}
